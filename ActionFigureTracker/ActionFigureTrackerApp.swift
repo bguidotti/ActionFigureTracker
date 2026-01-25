02f036d454f74fe68f615a487db333fbbef280c1
Bruce Guidotti <guidotti@gmail.com>
2026-01-25 16:43:23 -0600

    Initial Commit

---
+++ ActionFigureTracker/ActionFigureTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionFigureTrackerApp.swift
+//  ActionFigureTracker
+//
+//  Created by guidotti on 1/25/26.
+//
+
+import SwiftUI
+
+@main
+struct ActionFigureTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
